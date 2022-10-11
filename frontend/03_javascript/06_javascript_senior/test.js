366baf94865bbdd8b31807e43bc0dca66347886f
shadow <shadow_1218@icloud.com>
2022-10-12 02:08:23 +0800

    memoir

--- frontend/03_javascript/06_javascript_senior/test.js
+++ frontend/03_javascript/06_javascript_senior/test.js
@@ -1,8 +1,6 @@
-function foo() {}
+let str = 'shadow'
 
-console.log(foo.prototype.__proto__ === Object.prototype)
-console.log(Function.__proto__ === Function.prototype)
-console.log(Object.__proto__ === Function.prototype)
-console.log(foo.__proto__ === Function.prototype)
-console.log(Function.prototype.__proto__ === Object.prototype)
-console.log(Object.prototype.__proto__ === null)
+var nick = 'iris'
+
+console.log(window.nick)
+console.log(window.str)
